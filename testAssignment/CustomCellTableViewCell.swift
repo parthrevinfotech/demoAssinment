0cbaacef2073ffd21c894fc59b781e54da35d63b
Hiteshi <hiteshi@Administrators-MacBook-Pro-2.local>
2019-10-21 12:58:33 +0530

    first commit

---
+++ testAssignment/CustomCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCellTableViewCell.swift
+//  testAssignment
+//
+//  Created by Hiteshi on 21/10/19.
+//  Copyright © 2019 demo. All rights reserved.
+//
+
+import UIKit
+
+class CustomCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ testAssignment/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  testAssignment
+//
+//  Created by Hiteshi on 21/10/19.
+//  Copyright © 2019 demo. All rights reserved.
+//
+
+import Foundation
